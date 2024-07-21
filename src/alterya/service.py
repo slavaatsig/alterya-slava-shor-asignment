975adb42699f4ffb8b76cd261733b7b8b9e5d7a7
Slava Shor <slava.shor.il@gmail.com>
2024-07-21 16:05:01 +0300

    Upgrade service

--- src/alterya/service.py
+++ src/alterya/service.py
@@ -36,12 +36,46 @@ async def read_version():
     return {"version": "0.0.0"}
 
 
-@app.get("/v1/asset/token/list/wallet/{wallet}/chain/{chain}")
-async def read_chain_token_list(
+@app.get("/v1/wallet/{wallet}/chain/{chain}/tokens")
+async def read_wallet_chain_tokens(
     wallet: str, chain: str, covalent: CovalentHQApi = Depends(get_client)
 ):
     try:
-        return await covalent.list_chain_tokens(wallet=wallet, chain=chain)
+        return await covalent.list_wallet_chain_tokens(wallet=wallet, chain=chain)
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(
+            status_code=e.response.status_code,
+            detail="Remote service error: Unable to fetch data",
+        )
+    except Exception as _:
+        raise HTTPException(status_code=500, detail="Internal Server Error: Unable to parse data")
+
+
+@app.get("/v1/wallet/{wallet}/chain/{chain}/transactions/{page}")
+async def read_wallet_chain_transactions_paged(
+    wallet: str, chain: str, page: int, covalent: CovalentHQApi = Depends(get_client)
+):
+    try:
+        return await covalent.list_wallet_chain_tokens(wallet=wallet, chain=chain, page=page)
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(
+            status_code=e.response.status_code,
+            detail="Remote service error: Unable to fetch data",
+        )
+    except Exception as _:
+        raise HTTPException(status_code=500, detail="Internal Server Error: Unable to parse data")
+
+
+@app.get("/v1/wallet/{wallet}/chain/{chain}/balance/{currency}")
+async def read_wallet_chain_usd_balance(
+    wallet: str,
+    chain: str,
+    currency: str,
+    covalent: CovalentHQApi = Depends(get_client),
+):
+    try:
+        assert str(currency).lower() == "usd", "Currently ony USD is supported"
+        return await covalent.get_wallet_chain_usd_balance(wallet=wallet, chain=chain, currency=currency)
     except httpx.HTTPStatusError as e:
         raise HTTPException(
             status_code=e.response.status_code,
